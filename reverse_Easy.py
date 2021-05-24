174fde00a060bdb9db35ee78e8e707ee66a8c476
Mayur Deshmukh <37608608+mayurdeshmukh2104@users.noreply.github.com>
2021-05-24 21:32:03 +0530

    Reverse Integer - https://leetcode.com/problems/reverse-integer/submissions/ - for reference

---
+++ reverse_Easy.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        x = str(x)
+        if x[0] == '-':
+            a = int('-' + x[-1:0:-1])
+            if a >= -2147483648 and a<= 2147483647:
+                return a
+            else:
+                return 0
+        else:
+            a = int(x[::-1])
+            if a >= -2147483648 and a<= 2147483647:
+                return a
+            else:
+                return 0
+        
+        
+s = Solution()
+k = s.reverse(-123)
+print(k)
